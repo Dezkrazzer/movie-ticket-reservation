af26c4d612a8ca328120da0e9b4c59994031f9ed
Dezkrazzer <uqie.vanmadiun@gmail.com>
2025-05-10 21:37:37 +0700

    feat: adding website

--- src/main/java/dezkrazzer/movie_ticket_reservation/WebApp.java
+++ src/main/java/dezkrazzer/movie_ticket_reservation/WebApp.java
@@ -1,12 +1,17 @@
 package dezkrazzer.movie_ticket_reservation;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WebApp {
-    @GetMapping("/")
+    @GetMapping("/debugging")
     public String hello() {
         return "Welcome to Movie Ticket Reservation Web App!";
+    }
+
+    @GetMapping("/")
+    public String showIndex() {
+        return "index";
     }
 }
