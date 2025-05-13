9d4802bd3dc1bbc139db7059d2133b1332cb7426
Dezkrazzer <uqie.vanmadiun@gmail.com>
2025-05-13 14:03:43 +0700

    feat: Added category menu

---
+++ src/main/resources/static/js/index/category-movie.js
@@ -0,0 +1,4 @@
+    function scrollCategories(distance) {
+      const container = document.getElementById('categoryContainer');
+      container.scrollBy({ left: distance, behavior: 'smooth' });
+    }
